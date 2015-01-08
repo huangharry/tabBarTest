749d0ce610e752179767eafee2fda50239411c2e
Harry <huangharry19@gmail.com>
2015-01-08 12:18:31 +0800

    完成自訂抽籤，但刪除部分有問題以及無法輸入中文

---
+++ tabBarTest/SecondTableViewController.swift
@@ -0,0 +1,65 @@
+//
+//  SecondTableViewController.swift
+//  tabBarTest
+//
+//  Created by Harry on 2015/1/7.
+//  Copyright (c) 2015年 huangharry. All rights reserved.
+//
+
+import UIKit
+
+class SecondTableViewController: UIViewController {
+    
+    var result = [Int]()
+    var customResult = [String]()
+    var count = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return customResult.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell") as UITableViewCell
+        cell.textLabel?.text = String(customResult[indexPath.row])
+        return cell
+    }
+    
+    
+    func tableView(tableView: UITableView!, canEditRowAtIndexPath indexPath: NSIndexPath!) -> Bool {
+        return true
+    }
+    
+    func tableView(tableView: UITableView!, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath!) {
+        if (editingStyle == UITableViewCellEditingStyle.Delete) {
+            // handle delete (by removing the data from your array and updating the tableview)
+            if let deleteCell=tableView
+            {
+                customResult.removeAtIndex(indexPath!.row)
+                deleteCell.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+            }
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- tabBarTest/SecondViewController.swift
+++ tabBarTest/SecondViewController.swift
@@ -10,6 +10,27 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var secondInput: UITextField!
+   // var result = [Int]()
+    var customResult = [String]()
+    var count = 0 //計算使用者輸入的幾筆資料
+    
+    @IBAction func next(sender: AnyObject) {
+        if secondInput.text.isEmpty {
+            let alert = UIAlertView()
+            alert.title = "Imput empty"
+            alert.message = "請輸入"
+            alert.addButtonWithTitle("OK")
+            alert.show()
+        }
+        
+        else{
+            customResult.append(secondInput.text)
+            count++
+            println(customResult)
+            secondInput.text = ""
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,6 +40,39 @@ class SecondViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func shouldPerformSegueWithIdentifier(identifier: String?, sender: AnyObject?) -> Bool {
+        if identifier == "seeResult2" {
+            if secondInput.text.isEmpty  && count == 0{
+                let alert = UIAlertView()
+                alert.title = "Imput empty"
+                alert.message = "請輸入"
+                alert.addButtonWithTitle("OK")
+                alert.show()
+                
+                return false
+            }
+            else {
+                    for i in 0..<count {
+                        let random = Int(arc4random_uniform(UInt32(count)))
+                        var temp = customResult[random]
+                        customResult[random] = customResult[i]
+                        customResult[i] = temp
+                    }
+                    //println(result)
+                    return true
+            }
+        }
+        return false
+    }
+
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "seeResult2" {
+            var resultPage = segue.destinationViewController as SecondTableViewController
+            resultPage.customResult = self.customResult
+        }
+    }
+
 
 
 }
